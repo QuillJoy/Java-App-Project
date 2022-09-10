6db45a51ecdf55d8745f2877a8299af4d0f95b15
unknown <jwquill81@gmail.com>
2022-09-10 15:56:02 -0700

    Add basic menu

--- Main.java
+++ Main.java
@@ -1,6 +1,28 @@
 import java.util.*;
+
 public class Main {
     public static void main (String args[]){
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Menu: \n 1. Add new workout \n 2. Add progress \n 3. View workouts \n 4. Close");
+
+        int usrInput = scanner.nextInt();
+
+
+        while (usrInput != 4){
+            switch(usrInput){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3: 
+                    break;
+                case 4:
+                    return;
+            }
+            usrInput = scanner.nextInt();
+        }
+        System.out.println("Program closing...");
+
     }
 }
