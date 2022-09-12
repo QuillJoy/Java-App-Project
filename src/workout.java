69823d32857be6f5350d84a9ce39634f08055962
unknown <jwquill81@gmail.com>
2022-09-11 19:03:06 -0700

    Add HashMap object to workout class

--- src/workout.java
+++ src/workout.java
@@ -1,6 +1,7 @@
+import java.util.*;
 public class workout {
     String name;
-
+    HashMap<String, Integer> progress = new HashMap<String, Integer>();
 
 
     workout(){
@@ -16,7 +17,13 @@ public class workout {
         this.name = name;
     }
 
+    public void setProgress (String string, Integer integer){
+        progress.put(string,integer);
+    }
 
+    public HashMap<String,Integer> getProgress(){
+        return progress;
+    }
     
     
 }
